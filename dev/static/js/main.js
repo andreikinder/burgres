6b9985b6f3905ddc20ed3703388bbd69b6076b2a
andreikinder <42671896+AndreiKinder@users.noreply.github.com>
2019-03-19 22:11:04 +0500

    gallery

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,19 +1,39 @@
 $(document).ready(function () {
     svg4everybody({});
 	 $('.slider').slick({
+	  mobileFirst: true,
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
-	  arrows: false,
-	  fade: true,
-	  asNavFor: '.preview'
+	  arrows: true,
+      dots: false,
+	  asNavFor: '.preview',
+	  adaptiveHeight: true,
+	  responsive: [
+	  	{
+	      breakpoint: 767,
+	      settings: {
+	        arrows: false,
+	        fade: true,
+	      }
+	    }
+	  ]
 	});
 	$('.preview').slick({
-	  slidesToShow: 2,
-	  slidesToScroll: 1,
+	  mobileFirst: true,
+	  slidesToShow: 3,
 	  asNavFor: '.slider',
 	  dots: true,
 	  centerMode: true,
-	  focusOnSelect: true
+	  focusOnSelect: true,
+	   responsive: [
+	  	{
+	      breakpoint: 991,
+	      settings: {
+	        slidesToShow: 5
+	      }
+	    }
+	  ]
+
 	});
 
 
